33f36337a812f7a1bfef8d02bae5ec78ab9ba3f7
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-04-17 16:40:12 +0600

    user created

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import SocialLogin from '../../SocialLogin/SocialLogin';
 
 const Login = () => {
@@ -8,11 +10,23 @@ const Login = () => {
     const passwordRef = useRef('');
     const navigate = useNavigate();
 
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useSignInWithEmailAndPassword(auth);
+
+      if (user){
+          navigate('/home');
+      }
+
     const handleSubmit = event => {
         event.preventDefault();
         const email = emailRef.current.value;
         const password = passwordRef.current.value;
-        console.log(email, password);
+        signInWithEmailAndPassword(email,password);
+        // console.log(email, password);
     }
 
     const navigateToRegister = event => {
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './Register.css';
+import auth from '../../firebase.init';
 
 const Register = () => {
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
+
     const navigate = useNavigate();
 
     const navigateToLogin = () =>{
         navigate('/login');
+    }
+
+    if (user){
+        navigate('/home');
     }
 
     const handleRegister = event => {
@@ -15,7 +29,9 @@ const Register = () => {
         const name = event.target.name.value;
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(name, email, password)
+        // console.log(name, email, password)
+
+        createUserWithEmailAndPassword(email, password);
     }
 
     return (
