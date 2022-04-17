3dfd8202930538a294554ba69b6191cbe3c222be
Mousumi Mou <ridimarahmanmou5804@gmail.com>
2022-04-17 15:59:20 +0600

    login register design added

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Register.css';
 
 const Register = () => {
+    const navigate = useNavigate();
+
+    const navigateToLogin = () =>{
+        navigate('/login');
+    }
+
+    const handleRegister = event => {
+        event.preventDefault()
+
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        console.log(name, email, password)
+    }
+
     return (
         <div className='register-form-container'>
             <h2>Please Register</h2>
-            <form>
-                <input type="text" name="name" id="" placeholder='your Name' />
+            <form onSubmit={handleRegister}>
+                <input type="text" name="name" id="" placeholder='Enter Your Name' />
 
-                <input type="email" name="email" id="" placeholder='Email Address' required />
+                <input type="email" name="email" id="" placeholder='Enter YourEmail' required />
 
-                <input type="password" name="password" id="" placeholder='Password' required />
+                <input type="password" name="password" id="" placeholder='Enter Your Password' required />
 
                 <input type="checkbox" name="terms" id="terms" />
                 <label htmlFor="terms">Are you agree about these condition</label>
@@ -21,7 +36,7 @@ const Register = () => {
                 value="Register" />
 
             </form>
-            <p>Already have an account? <Link to='/login' className='text-primary pe-auto text-decoration-none' >Please Login</Link></p>
+            <p>Already have an account? <Link to='/login' className='text-primary pe-auto text-decoration-none' onClick={navigateToLogin} >Please Login</Link></p>
             
         </div>
     );
